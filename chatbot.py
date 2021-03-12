5ba281eaca60491f742a038c55accbc1f2744566
juan rios <juan.rios49@eia.edu.co>
2021-03-12 17:15:39 -0500

    creacion reconocimiento de voz con specch_recognition chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Di algo...")
+
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+
+    try:
+
+        text = r.recognize_google(audio,language='es-ES')
+        print("Dijiste: {}".format(text))
+
+    except:
+        print("No entiendo ")
