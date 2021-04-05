5397110c2468b3bbbbc8c780b1878003194a7df2
juan rios <juan.rios49@eia.edu.co>
2021-04-05 18:02:49 -0500

    chatbot, con condicionales y reproduccion audios JER,JFC

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,9 @@
+import pyglet
 import speech_recognition as sr
 
+
 r=sr.Recognizer()
+
 
 with sr.Microphone() as source:
     print("Di algo...")
@@ -10,8 +13,31 @@ with sr.Microphone() as source:
 
     try:
 
-        text = r.recognize_google(audio,language='es-ES')
-        print("Dijiste: {}".format(text))
+        text = r.recognize_google(audio, language='es-ES')
 
-    except:
-        print("No entiendo ")
+        print("Dijiste: {}".format(text.lower()))
+
+        if text.lower() == "hola pascal":
+            print("En que te puedo Ayudar")
+
+            song = pyglet.media.load('audio.mp3')
+            song.play()
+            pyglet.app.run()
+
+        if text.lower() == "prueba chat":
+            song = pyglet.media.load('file.mp3')
+            song.play()
+            pyglet.app.run()
+
+        if text.lower() == "semillero":
+            song = pyglet.media.load('test.mp3')
+            song.play()
+            pyglet.app.run()
+
+    except :
+        print("No entiendo err: {}")
+
+
+
+
+
