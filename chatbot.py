b817bcbd210278c683e973b17d2edaf021753c70
juan rios <juan.rios49@eia.edu.co>
2021-04-09 13:15:39 -0500

    playAudios JE JF

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,32 @@
-import pyglet
+import wave
 import speech_recognition as sr
+import alsaaudio
 
+
+def playAudio(self):
+    # open audio file and device
+    audio_file = wave.open(self, 'rb')
+    audio_device = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL, 'default')
+
+    # we are hard coding the audio format!
+    audio_device.setchannels(2)
+    audio_device.setrate(44100)
+    audio_device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+    audio_device.setperiodsize(980)
+
+    # play the audio
+    audio_data = audio_file.readframes(980)
+    while audio_data:
+        audio_device.write(audio_data)
+        audio_data = audio_file.readframes(980)
+
+    audio_file.close()
+
+# Class for blinking the leds
+
+#def escucharPascal(self):
 
 r=sr.Recognizer()
-
 
 with sr.Microphone() as source:
     print("Di algo...")
@@ -20,22 +43,23 @@ with sr.Microphone() as source:
         if text.lower() == "hola pascal":
             print("En que te puedo Ayudar")
 
-            song = pyglet.media.load('audio.mp3')
-            song.play()
-            pyglet.app.run()
+            playAudio('file.wav')
+
+            print("AUDIO END")
 
         if text.lower() == "prueba chat":
-            song = pyglet.media.load('file.mp3')
-            song.play()
-            pyglet.app.run()
+            playAudio('audio.wav')
+
+            print("AUDIO END")
 
         if text.lower() == "semillero":
-            song = pyglet.media.load('test.mp3')
-            song.play()
-            pyglet.app.run()
+            playAudio('file.wav')
+
+            print("AUDIO END")
 
     except :
-        print("No entiendo err: {}")
+        print("No entiendo")
+
 
 
 
